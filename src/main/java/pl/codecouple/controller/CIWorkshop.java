a50b5b219fdec1bff5f075cbdfeb0ce7f6603562
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-06-23 12:54:19 +0200

    Properties

--- src/main/java/pl/codecouple/controller/CIWorkshop.java
+++ src/main/java/pl/codecouple/controller/CIWorkshop.java
@@ -25,6 +25,7 @@ public class CIWorkshop {
 
     @RequestMapping("/books")
     public List<Book> getBooks(){
+//        bookRepository.save(new Book("Jan nowak"));
         return bookRepository.findAll();
     }
 
