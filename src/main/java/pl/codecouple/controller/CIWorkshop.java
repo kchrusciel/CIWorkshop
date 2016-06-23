04f0bc26140d28f3d2fbe4042eee589d1ef336a4
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-06-23 09:15:31 +0200

    Add mongo controller

--- src/main/java/pl/codecouple/controller/CIWorkshop.java
+++ src/main/java/pl/codecouple/controller/CIWorkshop.java
@@ -1,14 +1,32 @@
 package pl.codecouple.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.codecouple.domain.Book;
+import pl.codecouple.repository.BookRepository;
+
+import java.util.List;
 
 /**
  * Created by Krzysztof Chruściel.
  */
-@Controller
+@RestController
 public class CIWorkshop {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public CIWorkshop(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public List<Book> getBooks(){
+        return bookRepository.findAll();
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
